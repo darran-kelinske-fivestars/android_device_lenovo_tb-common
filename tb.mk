af9cf84a8299849ba07c9e2a0817275aa2f11f8f
darran-kelinske-fivestars <49406887+darran-kelinske-fivestars@users.noreply.github.com>
2020-04-29 08:07:12 -0500

    Add default radio package

--- tb.mk
+++ tb.mk
@@ -299,7 +299,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rild
 
-PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \       
+    android.hardware.radio@1.0
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
