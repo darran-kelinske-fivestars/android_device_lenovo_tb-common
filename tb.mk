9923ef3796042d421be772af62b4fefd670b4fc5
darran-kelinske-fivestars <49406887+darran-kelinske-fivestars@users.noreply.github.com>
2020-04-29 08:17:35 -0500

    Don't forget separator

--- tb.mk
+++ tb.mk
@@ -300,7 +300,7 @@ PRODUCT_PACKAGES += \
     rild
 
 PRODUCT_PACKAGES += \       
-    android.hardware.radio@1.0
+    android.hardware.radio@1.0 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
